faa1d3836118aac296ce0b8636f0f3267145723e
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2026-03-15 15:41:00 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,9 +1,42 @@
-import LoginForm from "./LoginForm"
+import { useState } from "react"
 
 export default function LoginPage(){
+
+    // state
+
+    const [inputValue, setInputValue] = useState("")
+
+    // comportements
+    const handleSubmit = (event) => {
+      event.preventDefault
+      alert(`Bonjour ${inputValue}`)
+      setInputValue("")
+    }
+    
+    const handleChange = (event) => {
+      setInputValue(event.target.value)
+    }
+    
+    // render
+
+
     return(
         <>
-        <LoginForm />
+        <h1>BIENVENUE CHEZ NOUS</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+            <input
+             value={inputValue}
+             onChange={handleChange}
+             type="text"
+             placeholder="Entrez votre prénom..."
+             required
+             />
+            <button type="submit">
+                Accédez à votre espace
+            </button>
+        </form>
         </>
     )
 }
