6e8572850fdf71cd9e54f768fd9018f6feac5e60
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2026-05-08 14:48:40 +0200

    🚧 : F05 > ORDER > CARD MENU > add grid layout

---
+++ src/components/pages/order/Main/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import styles from "../Cards/Cards.module.css"
+import Card from "./Card"
+
+
+export default function Cards() {
+  return (
+    <div className={styles.menuCards}>
+           <Card />
+    </div>
+  )
+}
--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,5 +1,5 @@
 import styles from "../Main/Main.module.css"
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu.js"
+import Cards from "./Cards/Cards.jsx"
 
 export default function Main() {
 
@@ -9,48 +9,7 @@ export default function Main() {
           <div className={styles.basket}>
             Basket
           </div>
-          <div className={styles.menuCards}>
-            <div className={styles.card}>Cards
-              <div>
-                <img src={fakeMenu2[0].imageSource}  alt="" />
-              </div>
-              <div className={styles.info}>
-                <div className={styles.title}>{fakeMenu2[0].title}</div>
-                <div className={styles.description}>description</div>
-              </div>
-            </div>
-
-            <div className={styles.card}>Cards
-              <div>Image
-                <img  alt="" />
-              </div>
-              <div className={styles.info}>
-                <div className={styles.title}>title</div>
-                <div className={styles.description}>description</div>
-              </div>
-            </div>
-
-            <div className={styles.card}>Cards
-              <div>Image
-                <img  alt="" />
-              </div>
-              <div className={styles.info}>
-                <div className={styles.title}>title</div>
-                <div className={styles.description}>description</div>
-              </div>
-            </div>
-
-            <div className={styles.card}>Cards
-              <div>Image
-                <img  alt="" />
-              </div>
-              <div className={styles.info}>
-                <div className={styles.title}>title</div>
-                <div className={styles.description}>description</div>
-              </div>
-            </div>
-        
-          </div>
+          <Cards />
      </main>
   )
 }
