e85ac755838afe9138c2b9af1a8ffa874ed650b3
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2026-05-08 13:05:19 +0200

    🚧 F05 : ORDER > MAIN > add grid layout + files  + create cars et basket components

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,9 +1,56 @@
 import styles from "../Main/Main.module.css"
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu.js"
 
 export default function Main() {
+
+
   return (
      <main className={styles.main}>
-          
+          <div className={styles.basket}>
+            Basket
+          </div>
+          <div className={styles.menuCards}>
+            <div className={styles.card}>Cards
+              <div>
+                <img src={fakeMenu2[0].imageSource}  alt="" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.title}>{fakeMenu2[0].title}</div>
+                <div className={styles.description}>description</div>
+              </div>
+            </div>
+
+            <div className={styles.card}>Cards
+              <div>Image
+                <img  alt="" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.title}>title</div>
+                <div className={styles.description}>description</div>
+              </div>
+            </div>
+
+            <div className={styles.card}>Cards
+              <div>Image
+                <img  alt="" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.title}>title</div>
+                <div className={styles.description}>description</div>
+              </div>
+            </div>
+
+            <div className={styles.card}>Cards
+              <div>Image
+                <img  alt="" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.title}>title</div>
+                <div className={styles.description}>description</div>
+              </div>
+            </div>
+        
+          </div>
      </main>
   )
 }
