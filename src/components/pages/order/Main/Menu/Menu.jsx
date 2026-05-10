c578beac3d71cf42aeb800190e5785241280e28c
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2026-05-10 17:58:22 +0200

    ♻️ F05 : > ORDER > MENU > change Card to ProductCard

--- src/components/pages/order/Main/Menu/Menu.jsx
+++ src/components/pages/order/Main/Menu/Menu.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import { fakeMenu2 } from "../../../../../fakeData/fakeMenu";
 import styles from "../Menu/Menu.module.css"
-import Card from "./Card"
+import ProductCard from "./ProductCard"
 
 
 export default function Menu() {
+
+   const [menu, setMenu] = useState(fakeMenu2);
+
   return (
     <div className={styles.menuCards}>
-           <Card />
+      {menu.map((produit) =>(
+           <ProductCard
+              key={produit.id} 
+              title={produit.title}
+              imageSource={produit.imageSource} 
+              price={produit.price} /> 
+      ))}
     </div>
   )
 }
---
+++ src/components/pages/order/Main/Menu/ProductCard.jsx
@@ -0,0 +1,22 @@
+import styles from "../Menu/ProductCard.module.css";
+
+
+export default function Card({title, imageSource, price}) {
+
+  return (
+            <div className={styles.card} >
+                <div>
+                    <img src={imageSource}  alt="produit menu" className={styles.image} />
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.title}>{title}</div>
+                    <div className={styles.infoText}>
+                        <div className={styles.prix}>{price}</div>
+                        <button className={styles.addButton}>
+                            Ajouter
+                        </button>
+                    </div>
+                </div>
+            </div>
+  )
+}
